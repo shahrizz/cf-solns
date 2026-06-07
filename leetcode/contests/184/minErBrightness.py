b406b5a6b8aadd7c6c8d3a13bb6e9f3b7e32f777
Shahriz <shahmarks2208@gmail.com>
2026-06-07 12:51:14 +0530

    Complete Files for Biweekly Contest 184 1 easy 2 medium

---
+++ leetcode/contests/184/minErBrightness.py
@@ -0,0 +1,21 @@
+n = 2
+brightness = 1
+intervals = [[0, 0], [2, 2]]
+intervals.sort()
+
+active_time = 0
+
+start, end = intervals[0]
+
+for s, e in intervals[1:]:
+    if s <= end + 1:
+        end = max(end, e)
+    else:
+        active_time += end - start + 1
+        start, end = s, e
+
+active_time += end - start + 1
+
+bulbs_needed = (brightness + 2) // 3
+
+print(bulbs_needed * active_time)
